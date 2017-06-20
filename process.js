72568cb0ac0d03ec0b0d80e6e3a10a5d1fb36881
quai20 <quai20@gmx.fr>
2017-06-20 10:00:15 +0200

    add world black canvas

--- process.js
+++ process.js
@@ -16,9 +16,18 @@ function initDemoMap(){
         }
     );
 
+    var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+	     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	      subdomains: 'abcd',
+	       minZoom: 0,
+	        maxZoom: 20,
+	         ext: 'png'
+});
+
     var baseLayers = {
         "Satellite": Esri_WorldImagery,
-        "Grey Canvas": Esri_DarkGreyCanvas
+        "Grey ": Esri_DarkGreyCanvas,
+        "Black" : Stamen_Toner
     };
 
     var map = L.map('map', {
