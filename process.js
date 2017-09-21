7af7aa4cc14761cebbdc1d3b1d7acdf670d060de
quai20 <quai20@gmx.fr>
2017-09-21 15:45:27 +0200

    Comments

--- process.js
+++ process.js
@@ -1,11 +1,11 @@
 
 function initDemoMap(){
-
+//BASE TILE LAYER 1
   var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
     'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
   });
-
+//BASE TILE LAYER 2
   var Esri_DarkGreyCanvas = L.tileLayer(
     "http://{s}.sm.mapstack.stamen.com/" +
     "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
@@ -15,7 +15,7 @@ function initDemoMap(){
       'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
     }
   );
-
+//BASE TILE LAYER 3
   var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by Stamen Design, CC BY 3.0 &mdash; Map data &copy; OpenStreetMap',
     subdomains: 'abcd',
@@ -23,26 +23,26 @@ function initDemoMap(){
     maxZoom: 20,
     ext: 'png'
   });
-
+//BASE TILE GROUP LAYER
   var baseLayers = {
     "Satellite": Esri_WorldImagery,
     "Grey ": Esri_DarkGreyCanvas,
     "Black" : Stamen_Toner
   };
-
+//MAP STRUCTURE
   var map = L.map('map', {
     layers: [ Esri_WorldImagery ],
     minZoom : 3,
     worldCopyJump: true,
     inertia: false
   });
-
+//MENU CREATION
   var layerControl = L.control.layers(baseLayers);
   layerControl.addTo(map);
   map.setView([40, -20], 4);
-
+//MOUSE POSITION BOTTOM LEFT
   L.control.mousePosition().addTo(map);
-
+//CREDIT FOR LOPS LOGO
   var credctrl = L.controlCredits({
   image: "dist/lops.png",
   link: "dist/lops.png",
@@ -50,23 +50,25 @@ function initDemoMap(){
   width: 96,
   height: 88
   }).addTo(map);
-
+//INIT RETURN FUNCTION
   return {
     map: map,
     layerControl: layerControl
   };
 }
 
-// map creation
+// MAP CREATION
 var mapStuff = initDemoMap();
 var map = mapStuff.map;
+// MENU
 var layerControl = mapStuff.layerControl;
+//ICON FOR SELECTED FLOAT
 ico0 = {iconShape: 'doughnut', borderWidth: 4, borderColor: '#50f308'};
 var curmarker = L.marker([0,0],{icon: L.BeautifyIcon.icon(ico0)});
+//TRAJ LAYER, EMPTY AT START
 var majaxLayer=L.layerGroup();
 map.addLayer(majaxLayer);
-
-//Side Panel
+//SIDE PANEL
 var sidebar = L.control.sidebar('sidebar', {
   closeButton: true,
   position: 'left',
@@ -74,7 +76,7 @@ var sidebar = L.control.sidebar('sidebar', {
 });
 map.addControl(sidebar);
 
-//CHAMPS DE VITESSES
+//DATA LAYERS
 // AVISO
 $.getJSON('data/aviso.json', function (data) {
   var velocityLayer1 = L.velocityLayer({
@@ -141,14 +143,14 @@ for (var i = 0; i < mapdata.length; i++)
   else {
     var marker = L.marker([mapdata[i].latitude,mapdata[i].longitude],{title: mapdata[i].Platform,icon: L.BeautifyIcon.icon(ico2)});
   }
-  //popup sidebar
+  //ONCLIK, CALL SUBMARKERCLICK FUNCTION (SIDE PANEL + TRAJ)
   marker.on('click',L.bind(SubMarkerClick,null,mapdata[i]));
   marker.addTo(argomarkers);
 };
 htmlName4='<font color="blue">Argo floats : '+WDate+'</font> <a target="_blank" href="http://www.umr-lops.fr/SO-Argo/Home/"><img src="dist/info.png" height="15" width="15"></a>'
 layerControl.addOverlay(argomarkers, htmlName4);
 
-//ARGO7
+//ARGO 7 DAYS
 var mapdata2=Data_ARGO7;
 var argomarkers2 = L.layerGroup();
 for (var i = 0; i < mapdata2.length; i++)
@@ -159,7 +161,7 @@ for (var i = 0; i < mapdata2.length; i++)
   else {
     var marker = L.marker([mapdata2[i].latitude,mapdata2[i].longitude],{title: mapdata2[i].Platform,icon: L.BeautifyIcon.icon(ico2)});
   }
-  //popup sidebar
+  //ONCLIK, CALL SUBMARKERCLICK FUNCTION (SIDE PANEL + TRAJ)
   marker.on('click',L.bind(SubMarkerClick,null,mapdata2[i]));
   marker.addTo(argomarkers2);
 };
@@ -167,11 +169,14 @@ htmlName5='<font color="blue">Argo floats : 7 days</font> <a target="_blank" hre
 layerControl.addOverlay(argomarkers2, htmlName5);
 map.addLayer(argomarkers2);
 
-//GESTION DU PANNEAU LATERAL
+//SIDE PANEL MANAGEMENT
 function SubMarkerClick(smarker) {
+  //DOUGHNUT MARKER ON THE SELECTED FLOAT
   curmarker.setLatLng([smarker.latitude,smarker.longitude]);
   curmarker.addTo(map);
+  //CLEAR ANY EXISTING TRAJECTORIES
   majaxLayer.clearLayers();
+  //ERDDAP URLs
   ti=smarker.Time;
   pl=smarker.Platform;
   inst=smarker.Institution;
@@ -188,7 +193,7 @@ function SubMarkerClick(smarker) {
   "<br><b>LAST YEAR TRAJECTORY</b>" +
   "<br><img src=\""+trajurl+"\" alt=\"not available\"><br>");
   sidebar.show();
-  //TEST ACCES ERDAPP VIA AJAX
+  //TEST ACCES ERDAPP VIA AJAX FOR TRAJECTORIES
   $.ajax({
         //http://www.ifremer.fr/erddap/tabledap/ArgoFloats.geoJson?time%2Clatitude%2Clongitude&platform_number=%226901603%22&time%3E=2017-01-01T00%3A00%3A00Z&time%3C=2017-09-20T17%3A18%3A20Z
         //url:"http://www.ifremer.fr/erddap/tabledap/ArgoFloats.json?time%2Clatitude%2Clongitude&platform_number=%22"+pl+"%22&time%3E="+(Number(ti.substr(0,4))-1).toString()+"-"+ti.substr(4,2)+"-"+ti.substr(6,2)+"T"+ti.substr(8,2)+"%3A"+ti.substr(10,2)+"%3A"+ti.substr(12,2)+"Z&orderBy(%22time%22)",
@@ -206,12 +211,12 @@ function SubMarkerClick(smarker) {
           }
       });
 }
-//REMOVE MARKER
+//REMOVE MARKER AND TRAJ WHEN CLOSING PANEL
 sidebar.on('hide', function () {
      map.removeLayer(curmarker);
      majaxLayer.clearLayers();
  });
 
-//OUTIL DE RECHERCHE DANS LES FLOTTEURS
+//SEARCH TOOL
 var controlSearch = new L.Control.Search({layer: argomarkers2, initial: false, position:'topleft'});
 map.addControl( controlSearch );
